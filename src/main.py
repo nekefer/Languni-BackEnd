3e1ddbb43c9727a585dc75af9c99aed0d61ed541
nekefer <kloutsedaniel1@gmail.com>
2026-03-14 22:00:46 -0500

    Fix: CORS issue for the billing

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ app.add_middleware(
     allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allow_headers=["Content-Type"],
+    allow_headers=["Content-Type", "Authorization", "Accept", "X-Requested-With"],
 )
 
 
